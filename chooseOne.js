3eac425bb0ff89c232bfb87149814fdd3aa6524e
Connor Ring <crv9557@saintjoe.edu>
2015-02-10 22:51:45 -0500

    Removed useless code, renamed more variables and made the code have the ability to be called. All 4 dice are callable, although they are called all at once.

--- chooseOne.js
+++ chooseOne.js
@@ -1,23 +1,25 @@
 // Connor Ring Dice Roller Code
 // Edited February 10th, 2015
 // Version 1.2
-// The program is meant to pick a die and roll the dice, picking a side based on which kind of die was selected.
-
-var courses = [['Austgen','Grube','Heinzman','Herre','Hofer','Samano','Zekovic'],['Davis', 'Gainey', 'Klump', 'Postma']];
+// The program is meant to pick a die and roll the dice, picking a side based on which kind of die was selected. Removed a bit of code that no longer served any purpose, and renamed 'var courses' to 'var dice' to fit the assignment better.
 
 //Edited 'course' to be 'side' to be more in line with assignment; also spaced out each new set of sides to make the code easier to read.
-var courses = { "4-Side": ['Side 1','Side 2', 'Side 3', 'Side 4'],
-       	"5-Side": ['Side 1', 'Side 2', 'Side 3', 'Side 4', 'Side 5'],
-       	"6-Side": ['Side 1', 'Side 2', 'Side 3', 'Side 4', 'Side 5', 'Side 6'],
-       	"8-Side": ['Side 1', 'Side 2', 'Side 3', 'Side 4', 'Side 5', 'Side 6', 'Side 7', 'Side 8']};
+var dice ={ "4-Side": ['Side 1' ,'Side 2', 'Side 3', 'Side 4'],
+       	    "5-Side": ['Side 1', 'Side 2', 'Side 3', 'Side 4', 'Side 5'],
+       	    "6-Side": ['Side 1', 'Side 2', 'Side 3', 'Side 4', 'Side 5', 'Side 6'],
+       	    "8-Side": ['Side 1', 'Side 2', 'Side 3', 'Side 4', 'Side 5', 'Side 6', 'Side 7', 'Side 8']};
  
 // As mentioned earlier, changed 'courses' to 'sides'; also made the random variable be named 'roll'
 function chooseOne(sides) {
-  var thisClass  = courses[sides];
+  var thisDie  = dice[sides];
 
-  var n = thisClass.length;
+  var n = thisDie.length;
   var roll = Math.floor(Math.random() * n);
 
-  print('Chosen: ' + thisClass[roll]);
-}
+  print('Chosen: ' + thisDie[roll]);}
 
+//Call the function for each type of die
+chooseOne("4-Side");
+chooseOne("5-Side");
+chooseOne("6-Side");
+chooseOne("8-Side");
